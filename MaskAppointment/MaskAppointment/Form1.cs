3fc7d03b7533f5d52a2eb60028be4dc41cb8ff0b
spy2217 <60692023+spy2217@users.noreply.github.com>
2020-03-15 19:05:37 +0800

    Update Form1.cs
    中奖

--- MaskAppointment/MaskAppointment/Form1.cs
+++ MaskAppointment/MaskAppointment/Form1.cs
@@ -133,5 +133,101 @@ namespace MaskAppointment
         {
             register();
         }
+        public static void SetRewardPerson(int listnumber){
+            int mask = 0;
+            int listNumber = listnumber;
+            String date="";
+            String deadline = "";
+            String connectStr0 = "server=127.0.0.1;port=3306;user=root;password=123456;database=mask";
+            MySqlConnection conn0 = new MySqlConnection(connectStr0);
+            try
+            {
+                conn0.Open();
+                Console.WriteLine("数据已打开1");
+                string sql = "select * from appointment where listNumber = "+listNumber;
+                MySqlCommand cmd = new MySqlCommand(sql, conn0);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                reader.Read();
+                mask += reader.GetInt32("mask");
+                date += reader.GetString("closetime");
+                DateTime dateTime = Convert.ToDateTime(date);
+                dateTime.AddDays(3);
+                deadline = dateTime.ToShortDateString();
+                Console.WriteLine(deadline);
+             }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn0.Close();
+            }
+
+            int people = 0;
+            int maxpeople = 0;
+            int mask2=0;
+            String connectStr = "server=127.0.0.1;port=3306;user=root;password=123456;database=mask";
+            MySqlConnection conn = new MySqlConnection(connectStr);
+            String[] name = new String[100];
+            String[] ID = new String[100];
+            String[] phone = new String[100];
+            String[] registerID = new String[100];
+            int[] maskNumber = new int[100];
+            try
+            {
+                conn.Open();
+                Console.WriteLine("数据已打开2");
+                string sql = "select * from Register where listNumber = "+listNumber;
+                MySqlCommand cmd = new MySqlCommand(sql,conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+               for(int i=0; reader.Read();++i)
+                {
+                    name[i] = reader.GetString("name");
+                    ID[i] = reader.GetString("ID");
+                    phone[i] = reader.GetString("phone");
+                    maskNumber[i] = reader.GetInt32("maskNumber");
+                    registerID[i] = reader.GetString("registerID");
+                    people++;
+                }
+               for(int i=0;i<people;++i)
+                {
+                    mask2 += maskNumber[i];
+                    if (mask >= mask2) maxpeople++;
+                    else break;
+                }
+            }
+            catch(MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            String connectStr2 = "server=127.0.0.1;port=3306;user=root;password=123456;database=mask";
+            MySqlConnection conn2 = new MySqlConnection(connectStr2);
+            try
+            {
+                conn2.Open();
+                Console.WriteLine("数据已打开3");
+                string sql = "";
+                for (int i = 0; i<maxpeople; ++i)
+                {
+                    sql += "insert into list(listNumber,name,ID,phone,maskNumber,registerID,Date,deadline) values(" + listNumber + ",'" + name[i] + "','" + ID[i] + "','" + phone[i] + "'," + maskNumber[i] + ",'" + registerID[i] + "','" + date + "','" + deadline + "');";
+                }
+                MySqlCommand cmd = new MySqlCommand(sql,conn2);
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn2.Close();
+            }
+       }
     }
 }
